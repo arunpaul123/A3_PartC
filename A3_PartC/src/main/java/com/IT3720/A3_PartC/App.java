f63f6d3bf2d91c51b59cc8540663dc44a2515638
Arun <Arun@156.59.129.1>
2019-10-16 10:22:42 +1300

    Assignment3 Part3

---
+++ A3_PartC/src/main/java/com/IT3720/A3_PartC/App.java
@@ -0,0 +1,13 @@
+package com.IT3720.A3_PartC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
